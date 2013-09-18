bce7f5c0cf46f7bd9eb980afe312e6a1f2908fb4
Shantanu Kumar <kumar.shantanu@gmail.com>
2013-09-18 19:26:56 +0530

    add min lein version requirement

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/kumarshantanu/lein-cascade"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :eval-in-leiningen true)
